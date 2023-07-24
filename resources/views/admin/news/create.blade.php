60813e45b0e3a2e3aab9b59545f96ab371de62a6
takano <goyataiko@gmail.com>
2023-07-24 05:24:47 +0000

    06 layout作成、view修正

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>MyNewsCreate</title>
-    </head>
-    <body>
-        <h1>MyNewsCreate</h1>
-    </body>
-</html>
+@extends('layouts.admin')
+@section('title', 'MyNewsCreate')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>MyNewsCreate</h2>
+            </div>
+        </div>
+    </div>
+@endsection
